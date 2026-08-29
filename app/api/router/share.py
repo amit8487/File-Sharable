d3aa8635286f2eae55f543b2d88d27826d071c4e
Amit pal <amitpal8487@gmail.com>
2026-08-29 16:52:27 +0530

    updated Upload path

--- app/api/router/share.py
+++ app/api/router/share.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, UploadFile
+
+router = APIRouter(prefix="/share", tags=["ShareFile"])
+
+@router.post("/upload/")
+async def upload(files: list[UploadFile] | None = None):
+    file_info = {}
+   
+    if not files or files is None:
+        raise HTTPException(status_code=400, detail="No file Uploaded")
+    else:
+        for file in files:
+            content = await  file.read()
+            if(content == ""):
+                continue
+            file_info[file.filename] = len(content)
 
 
-router = APIRouter(prefix="/share", tags = ["ShareFile"])
-
-@router.post("/upload")
-async def download():
-    
+        return {"files": file_info}
