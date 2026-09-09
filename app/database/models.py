771696574f4e4615564f339a3ff64ea052778176
Amit pal <amitpal8487@gmail.com>
2026-09-10 02:22:47 +0530

    Update code of models.py to fixed some bugs

--- app/database/models.py
+++ app/database/models.py
@@ -1,25 +1,31 @@
-from enum import Enum
+from datetime import datetime, timezone
+from typing import List, Optional
 from uuid import UUID, uuid4
-from datetime import datetime
 
-from sqlmodel import SQLModel, Field, Relationship, column
+from sqlmodel import Field, Relationship, SQLModel
 
-
-class share(SQLModel, table = True):
+class Share(SQLModel, table = True):
+    __tablename__ = "shares"
     id: UUID = Field(primary_key=True, default_factory=uuid4)
     unique_code : str = Field(index=True, unique=True)
-    user_id: UUID = Field(default= None, default_factory= uuid4, foreign_key="user.id")
-    password_hash: str = Field(default=None)
+    user_id: Optional[UUID] = Field(default=None)
+    password_hash: Optional[str] = Field(default=None)
     expiry_at: datetime 
     created_at: datetime
-    download_limit: int = Field(default=None)
-    download_count: int = Field(default=None)
-    total_size: int
+    download_limit: int = Field(default=5)
+    download_count: int = Field(default=0)
+    total_size: int = Field(default=0)
+
+    files: List["File"] = Relationship(back_populates="share", cascade_delete=True)
 
 class File(SQLModel, table = True):
+    __tablename__ = "files"
     id: UUID = Field(primary_key=True, default_factory=uuid4)
-    share_id: UUID = Field(index=True, foreign_key="share.unique_code")
+    share_id: UUID = Field(foreign_key="shares.id", index=True)
     original_name:str
-    stored_filename:str(unique=True)
+    stored_filename:str = Field(unique=True)
+    file_size: int
     mime_type:str
-    created_at:datetime
+    created_at:datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    share: "Share" = Relationship(back_populates="files")
--- app/database/session.py
+++ app/database/session.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
+
 URL = "sqlite+aiosqlite:///database.db"
 
 engine = create_async_engine(
@@ -11,7 +12,7 @@ engine = create_async_engine(
 
 async def create_db_tables():
     async with engine.begin() as connection: #below import statement we will add name of class 
-        from app.database.models import # noqa: F401
+        from app.database.models import table, share# noqa: F401
         await connection.run_sync(SQLModel.metadata.create_all)
 
 async_session = sessionmaker(
