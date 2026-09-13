35f56ea6911b2f56c00dafe72b3565f97db7158b
Amit pal <amitpal8487@gmail.com>
2026-09-13 23:24:31 +0530

    j

--- app/database/session.py
+++ app/database/session.py
@@ -2,7 +2,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
-
 URL = "sqlite+aiosqlite:///database.db"
 
 engine = create_async_engine(
